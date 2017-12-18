bd6a10748427693fe08cf18bd855597b8fc85d00
jymassol.netmessage <jymassol@netmessage.com>
2017-12-18 17:34:44 +0100

    fix authentication

--- projet/app/scenes/Authentication/Login.js
+++ projet/app/scenes/Authentication/Login.js
@@ -23,14 +23,13 @@ export default class Login extends React.Component {
     }
 
     _login() {
-        if(this.state.password == this.state.verifyPassword) {
-            firebaseRef.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error){
-                console.log(error.code)
-                console.log(error.message)
-            })
-        } else {
-            console.log("Password did not match");
-        }
+        firebaseRef.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error){
+            console.log(error.code)
+            console.log(error.message)
+        })
+
+        Actions.pagecontrol();
+    
     }
 
     _register() {
